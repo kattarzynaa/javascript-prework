9c7ae3f966d5d1b40fd3bf5f1a69b80159cad1f8
Kasia Wetula <wetula.kasia@gmail.com>
2023-03-05 20:11:54 +0100

    Add basic game logic

--- js/script.js
+++ js/script.js
@@ -1,5 +1,65 @@
 
-let computerMove = 'kamień';
-let playerMove = 'papier';
 
-printMessage('Zagrałem ' + computerMove + '! ' + 'Jeśli Twój ruch to ' + playerMove + ' to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1 );
+
+console.log('Wylosowany numer: ' + randomNumber);
+
+let computerMove = '';
+
+if(randomNumber == 1){
+    computerMove = 'kamień';
+}
+else if(randomNumber == 2){
+    computerMove = 'papier';
+}
+else {
+    computerMove = 'nożyce';
+}
+
+printMessage('Ruch komputera: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój tuch! 1: kamień, 2: papier, 3: nożyce');
+
+console.log('Gracz wpisał: ' + playerInput)
+
+let playerMove = '';
+
+if(playerInput == '1') {
+    playerMove = 'kamień'
+}
+else if(playerInput == '2'){
+    playerMove = 'papier'
+}
+else  if(playerInput=='3'){
+    playerMove = 'nożyce';
+}
+else{
+    playerMove = 'ruch nieznany';
+}
+
+printMessage('Twój ruch: ' + playerMove);
+
+if (computerMove == 'kamień' && playerMove == 'papier') {
+    printMessage('WYGRAŁEŚ! :)')
+}
+else if(computerMove == 'papier' && playerMove == 'nożyce') {
+    printMessage('WYGRAŁEŚ :)');
+} 
+else if(computerMove == 'nożyce' && playerMove == 'kamień') {
+    printMessage('WYGRAŁEŚ :)');
+} 
+else if(computerMove == playerMove) {
+    printMessage('REMIS!');
+}
+else if(computerMove == 'nożyce' && playerMove == 'papier') {
+    printMessage('PRZEGRAŁEŚ :(');
+}
+else if(computerMove == 'papier' && playerMove == 'kamień') {
+    printMessage('PRZEGRAŁEŚ :(');
+}
+else if(computerMove == 'kamień' && playerMove == 'nożyce') {
+    printMessage('PRZEGRAŁEŚ :(');
+}
+else {
+    printMessage('Niedozwolony ruch, zagraj jeszcze raz!')
+}
